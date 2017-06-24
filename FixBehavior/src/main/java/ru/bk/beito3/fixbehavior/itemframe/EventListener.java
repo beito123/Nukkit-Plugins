c6f2ef050216613792c531a3659b6e52bdeea6cf
beito123 <beito123@gmx.com>
2017-06-24 22:58:11 +0900

    FixBehavior: Remove drop codes of ItemFrame
    I misunderstood... originally, it can drop :P
    so, remove drop codes of ItemFrame...

--- FixBehavior/src/main/java/ru/bk/beito3/fixbehavior/itemframe/EventListener.java
+++ FixBehavior/src/main/java/ru/bk/beito3/fixbehavior/itemframe/EventListener.java
@@ -6,18 +6,17 @@ import cn.nukkit.block.BlockItemFrame;
 import cn.nukkit.blockentity.BlockEntity;
 import cn.nukkit.blockentity.BlockEntityItemFrame;
 import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.server.DataPacketReceiveEvent;
 import cn.nukkit.item.Item;
 import cn.nukkit.level.Level;
-import cn.nukkit.math.BlockFace;
-import cn.nukkit.math.NukkitRandom;
 import cn.nukkit.math.Vector3;
 import cn.nukkit.network.protocol.ItemFrameDropItemPacket;
 
 public class EventListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onDataPacket(DataPacketReceiveEvent event) {
         if (event.getPacket().pid() == 0x47) {//ItemFrameDropPacket
             if (!(event.getPacket() instanceof ItemFrameDropItemPacket)) {
@@ -38,26 +37,7 @@ public class EventListener implements Listener {
 
                 if (itemframe.getItem().getId() == Item.AIR) {//Break the ItemFrame
                     level.useBreakOn(pos, player.getInventory().getItemInHand(), player, true);
-                    return;
                 }
-
-                NukkitRandom random = new NukkitRandom();
-                if ((random.nextRange(1, 100) / 100) <= itemframe.getItemDropChance()) {
-                    BlockFace face = ((BlockItemFrame) block).getFacing();
-
-                    Vector3 motion = null;
-                    if (face != null) {
-                        motion = new Vector3(
-                                ((double) -face.getXOffset() / 10) + ((double) random.nextRange(-10, 10) / 100),
-                                0.15,
-                                ((double) -face.getZOffset() / 10) + ((double) random.nextRange(-10, 10) / 100)
-                        );
-                    }
-                    level.dropItem(pos.add(0.5, 0.3, 0.5), itemframe.getItem(), motion);
-                }
-
-                itemframe.setItem(Item.get(Item.AIR));
-                itemframe.setItemRotation(0);
             }
         }
     }
